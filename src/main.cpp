387a05f1d2a6d8b559f748d52fd79960c18a154e
Wen Sun <syl1887415157@126.com>
2019-12-09 09:37:31 +0800

    feat: add error handling frame

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@ int main()
 {
     const string in_path = "../Test.sw";
     const string out_path = "../Test_out_lex.txt";
-    analyze_lex(in_path, out_path);
-    cout << "Finished. Tokens file path: " + out_path << endl;
+    bool successed = analyze_lex(in_path, out_path);
+    if (successed)
+    {
+        cout << "Finished. Tokens file path: " + out_path << endl;
+    }
 }
